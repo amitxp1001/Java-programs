3d6b07c393a3551d0a69030a926f30a8b7a783a4
amitxp1001 <114494738+amitxp1001@users.noreply.github.com>
2023-07-14 16:31:34 +0530

    Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
